c0cd7e365dc1e224b442e0f9c38ba07f138eb157
Pepa Pánek <josef.panek@gmail.com>
2022-05-05 00:55:55 +0200

    Add project files.

---
+++ Sorting_numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pole;
+            string[] poleRetezcu;
+            string zadanyRetezec;
+
+            Console.WriteLine("Zadej čísla pro seřazení (oddělená čárkou):");
+            zadanyRetezec = "0, -2, 10, 1, -8, 18, 3";// Console.ReadLine();
+
+            Console.WriteLine("Seřazená čísla: ");
+            Console.ReadKey();
+        }
+    }
+}
