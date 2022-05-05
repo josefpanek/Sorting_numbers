8591f5c25de05058a1b0efa1cc0eb1368249ff60
Pepa Pánek <josef.panek@gmail.com>
2022-05-05 10:48:14 +0200

    finished

--- Sorting_numbers/Program.cs
+++ Sorting_numbers/Program.cs
@@ -10,14 +10,40 @@ namespace Sorting_numbers
     {
         static void Main(string[] args)
         {
-            int[] pole;
             string[] poleRetezcu;
             string zadanyRetezec;
+            int min;
+            int temp;
 
             Console.WriteLine("Zadej čísla pro seřazení (oddělená čárkou):");
-            zadanyRetezec = "0, -2, 10, 1, -8, 18, 3";// Console.ReadLine();
+            zadanyRetezec = Console.ReadLine();
 
-            Console.WriteLine("Seřazená čísla: ");
+            poleRetezcu = zadanyRetezec.Split(','); //splitting the input string
+            int[] vysledkySerazene = new int[poleRetezcu.Length]; //initialisaton of the results array
+
+            for (int i = 0; i < poleRetezcu.Length; i++)    //parsing array of strings to array of ints
+                vysledkySerazene[i] = int.Parse(poleRetezcu[i]);
+
+            for (int i = 0; i < (vysledkySerazene.Length - 1); i++) //sorting algorithm found on itnetwork.cz
+            {
+                min = vysledkySerazene.Length - 1;
+                // hledani minima
+                for (int j = i; j < (vysledkySerazene.Length - 1); j++)
+                    if (vysledkySerazene[min] > vysledkySerazene[j])
+                        min = j;
+                // prohozeni prvku
+                temp = vysledkySerazene[min];
+                vysledkySerazene[min] = vysledkySerazene[i];
+                vysledkySerazene[i] = temp;
+            }
+
+            Console.WriteLine("Seřazená čísla: ");  //output of the result array
+            for (int i = 0; i < vysledkySerazene.Length; i++)
+            {
+                Console.Write(vysledkySerazene[i]);
+                if (i < vysledkySerazene.Length - 1)
+                    Console.Write(", ");
+            }
             Console.ReadKey();
         }
     }
